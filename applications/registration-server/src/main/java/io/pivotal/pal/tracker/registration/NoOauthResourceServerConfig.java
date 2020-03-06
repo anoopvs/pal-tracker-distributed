774545ba56085405697165362385d709476a52d9
V S <456482@cognizant.com>
2020-03-06 17:33:49 +0530

    Securing a Distributed System test fix 11

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/NoOauthResourceServerConfig.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/NoOauthResourceServerConfig.java
@@ -3,11 +3,11 @@ package io.pivotal.pal.tracker.registration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @ConditionalOnProperty(value = "application.oauth-enabled", havingValue = "false")
-public class NoOauthResourceServerConfig extends ResourceServerConfigurerAdapter {
+public class NoOauthResourceServerConfig extends WebSecurityConfigurerAdapter  {
 	
     public NoOauthResourceServerConfig() {
 		super();
@@ -16,6 +16,7 @@ public class NoOauthResourceServerConfig extends ResourceServerConfigurerAdapter
 
 	@Override
     public void configure(HttpSecurity http) throws Exception {
+		
         http
         .authorizeRequests()
         .anyRequest().permitAll()
