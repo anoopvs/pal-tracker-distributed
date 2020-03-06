560cf0a2eaecd1e23802ab0473a1b6911a029a48
V S <456482@cognizant.com>
2020-03-06 14:19:44 +0530

    Securing a Distributed System test fix 5

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/SecurityConfiguration.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/SecurityConfiguration.java
@@ -5,17 +5,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
+
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-            .anyRequest().permitAll()
-            .and()
-            .csrf().disable();
+//        http
+//            .authorizeRequests()
+//            .anyRequest().permitAll()
+//            .and()
+//            .csrf().disable();
     }
 
 }	
