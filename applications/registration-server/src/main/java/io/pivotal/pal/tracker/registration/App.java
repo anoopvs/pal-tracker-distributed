de85f056a9ce07158c0e4bbf2bed628a1823138c
V S <456482@cognizant.com>
2020-03-06 16:54:36 +0530

    Securing a Distributed System test fix 10

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -4,9 +4,12 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @EnableWebSecurity
 @SpringBootApplication
@@ -22,5 +25,17 @@ public class App {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(App.class, args);
+    }
+    
+    @Bean
+    @ConditionalOnProperty(value = "application.oauth-enabled", havingValue = "false")
+    public ResourceServerConfigurerAdapter resourceServerConfigurerAdapterTest(){
+    	return new NoOauthResourceServerConfig();
+    }
+    
+    @Bean
+    @ConditionalOnProperty(value = "application.oauth-enabled", matchIfMissing = true)
+    public ResourceServerConfigurerAdapter resourceServerConfigurerAdapterCloud(){
+    	return new OauthResourceServerConfig();
     }
 }
