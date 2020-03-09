cc448107e27cdf674d3009d2a49a396d003566a6
V S <456482@cognizant.com>
2020-03-09 14:56:43 +0530

    Securing a Distributed System test Retry1

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -4,16 +4,19 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+import java.util.TimeZone;
 
 @EnableWebSecurity
+@EnableResourceServer
+@EnableOAuth2Client
+@EnableEurekaClient
 @SpringBootApplication
 @ComponentScan({
     "io.pivotal.pal.tracker.accounts",
@@ -22,24 +25,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
     "io.pivotal.pal.tracker.users",
     "io.pivotal.pal.tracker.registration"
 })
-@EnableEurekaClient
 public class App {
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(App.class, args);
     }
-    
-//    @Bean
-//    @ConditionalOnProperty(value = "application.oauth-enabled", havingValue = "false")
-//    public ResourceServerConfigurerAdapter resourceServerConfigurerAdapterTest(){
-//    	System.err.println("resourceServerConfigurerAdapterTest>>>>");
-//    	return new NoOauthResourceServerConfig();
-//    }
-//    
-//    @Bean
-//    @ConditionalOnProperty(value = "application.oauth-enabled", matchIfMissing = true)
-//    public ResourceServerConfigurerAdapter resourceServerConfigurerAdapterCloud(){
-//    	System.err.println("resourceServerConfigurerAdapterCloud>>>>");
-//    	return new OauthResourceServerConfig();
-//    }
 }
