c3082a213a35aff855da65e030c343baf1093860
V S <456482@cognizant.com>
2020-03-09 12:35:46 +0530

    Securing a Distributed System test fix 13

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -23,7 +23,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
     "io.pivotal.pal.tracker.registration"
 })
 @EnableEurekaClient
-@EnableOAuth2Client
 @EnableResourceServer
 public class App {
     public static void main(String[] args) {
