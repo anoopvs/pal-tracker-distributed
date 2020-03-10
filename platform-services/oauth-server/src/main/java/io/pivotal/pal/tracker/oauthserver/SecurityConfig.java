70532a7543621887d92081911e9f07ae5419352d
V S <456482@cognizant.com>
2020-03-10 15:17:46 +0530

    Config server

--- platform-services/oauth-server/src/main/java/io/pivotal/pal/tracker/oauthserver/SecurityConfig.java
+++ platform-services/oauth-server/src/main/java/io/pivotal/pal/tracker/oauthserver/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig extends AuthorizationServerConfigurerAdapter {
         clients.inMemory()
             .withClient("tracker-client")
             .secret("supersecret")
-            .authorizedGrantTypes("client_credentials","oauth2-resource")
+            .authorizedGrantTypes("client_credentials")
             .scopes("openid");
     }
 
